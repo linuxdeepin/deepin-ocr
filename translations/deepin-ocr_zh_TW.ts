1a9d907f3732180c6375fe5112ee7e277402696d
liuminghang <liuminghang@uniontech.com>
2021-07-08 10:25:51 +0800

    feat: 增加繁体中文的翻译
    Description: 增加繁体中文的翻译
    
    Log: 增加繁体中文的翻译
    Change-Id: Iea01ba3dc6faa7390ed7ff8746861b10f445d10d

--- translations/deepin-ocr_zh_TW.ts
+++ translations/deepin-ocr_zh_TW.ts
@@ -5,64 +5,64 @@
     <name>MainWidget</name>
     <message>
         <source>Recognizing</source>
-        <translation type="unfinished"></translation>
+        <translation>正在識別</translation>
     </message>
     <message>
         <source>Copied</source>
-        <translation type="unfinished"></translation>
+        <translation>已複製</translation>
     </message>
     <message>
         <source>No text recognized</source>
-        <translation type="unfinished"></translation>
+        <translation>未識別到文字</translation>
     </message>
     <message>
         <source>Copy text</source>
-        <translation type="unfinished"></translation>
+        <translation>複製文字</translation>
     </message>
     <message>
         <source>Save as TXT</source>
-        <translation type="unfinished"></translation>
+        <translation>保存為TXT</translation>
     </message>
     <message>
         <source>Results</source>
-        <translation type="unfinished"></translation>
+        <translation>識別結果</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
         <source>OCR Tool</source>
-        <translation type="unfinished"></translation>
+        <translation>圖文識別工具</translation>
     </message>
 </context>
 <context>
     <name>ResultTextView</name>
     <message>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>複製</translation>
     </message>
     <message>
         <source>Cut</source>
-        <translation type="unfinished"></translation>
+        <translation>剪切</translation>
     </message>
     <message>
         <source>SelectAll</source>
-        <translation type="unfinished"></translation>
+        <translation>全選</translation>
     </message>
     <message>
         <source>Paste</source>
-        <translation type="unfinished"></translation>
+        <translation>粘貼</translation>
     </message>
 </context>
 <context>
     <name>Widget</name>
     <message>
         <source>Widget</source>
-        <translation type="unfinished"></translation>
+        <translation>視窗</translation>
     </message>
     <message>
         <source>Tips: The clearer the image is, the more accurate the text is</source>
-        <translation type="unfinished"></translation>
+        <translation>溫馨提示：影像越簡潔清晰，識別越準確</translation>
     </message>
 </context>
 </TS>
