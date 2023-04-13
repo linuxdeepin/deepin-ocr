c8bf52deed9a967a0462a0133ed9efac27e982c6
liwenke <winkli1@hust.edu.cn>
2023-04-14 04:09:51 +0800

    fix: export file delay
    use endl to push write buffer into the device (flush QTextStream)

--- src/mainwidget.cpp
+++ src/mainwidget.cpp
@@ -599,7 +599,7 @@ void MainWidget::slotExport()
     QFile file(path);
     if (file.open(QFile::WriteOnly | QFile::Text)) {
         QTextStream out(&file);
-        out << m_plainTextEdit->document()->toPlainText();
+        out << m_plainTextEdit->document()->toPlainText() << endl;
     }
 }
 
