ff4683712f8cc6195bd310f4e2df851b38bd68bf
dengzhongyuan365-dev <dengzhongyuan@uniontech.com>
2025-08-07 19:07:44 +0800

    chore: Add new Lao translation file (#67)
    * chore: Add new Lao translation file
    
    - Add new Lao translation file
    
    Log: Improve language support by adding Lao translation
    
    * chore: Update version to 6.5.8
    
     - update version to 6.5.8
    
     log: update version to 6.5.8

---
+++ translations/deepin-ocr_lo.ts
@@ -0,0 +1,102 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lo">
+<context>
+    <name>MainWidget</name>
+    <message>
+        <location filename="../src/mainwidget.cpp" line="118"/>
+        <source>No text recognized</source>
+        <translation>ບໍ່ມີຂໍ້ຄວາມທີ່ຮັບຮູ້</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwidget.cpp" line="169"/>
+        <source>Recognize language</source>
+        <translation>ຮັບຮູ້ພາສາ</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwidget.cpp" line="172"/>
+        <source>Simplified Chinese</source>
+        <translation>ຈີນງ່າຍ</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwidget.cpp" line="172"/>
+        <source>English</source>
+        <translation>ອັງກິດ</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwidget.cpp" line="172"/>
+        <source>Traditional Chinese</source>
+        <translation>ຈີນຢູ່ທີ່ສຸດ</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwidget.cpp" line="207"/>
+        <source>Copy text</source>
+        <translation>ຄັດລອກຂໍ້ຄວາມ</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwidget.cpp" line="213"/>
+        <source>Save as TXT</source>
+        <translation>ບັນທຶກເປັນ TXT</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwidget.cpp" line="239"/>
+        <source>Undo (Ctrl+Z)</source>
+        <translation>ຍົກເລີກ (Ctrl+Z)</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwidget.cpp" line="250"/>
+        <source>Redo (Ctrl+Shift+Z)</source>
+        <translation>ຍົກເລີກອີກຄັ້ງ (Ctrl+Shift+Z)</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwidget.cpp" line="320"/>
+        <location filename="../src/mainwidget.cpp" line="361"/>
+        <source>Recognizing</source>
+        <translation>ກຳລັງຮັບຮູ້</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwidget.cpp" line="589"/>
+        <source>Copied</source>
+        <translation>ຄັດລອກຂໍ້ຄວາມ</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="43"/>
+        <source>OCR Tool</source>
+        <translation>ເຄື່ອງມື OCR</translation>
+    </message>
+</context>
+<context>
+    <name>ResultTextView</name>
+    <message>
+        <location filename="../src/resulttextview.cpp" line="27"/>
+        <source>Copy</source>
+        <translation>ຄັດລອກຂໍ້ຄວາມ</translation>
+    </message>
+    <message>
+        <location filename="../src/resulttextview.cpp" line="29"/>
+        <source>Cut</source>
+        <translation>ຕັດ</translation>
+    </message>
+    <message>
+        <location filename="../src/resulttextview.cpp" line="32"/>
+        <source>Select All</source>
+        <translation>ເລືອກທັງໝົດ</translation>
+    </message>
+    <message>
+        <location filename="../src/resulttextview.cpp" line="34"/>
+        <source>Paste</source>
+        <translation>ເຈີບ</translation>
+    </message>
+</context>
+<context>
+    <name>Widget</name>
+    <message>
+        <location filename="../src/mainwidget.cpp" line="309"/>
+        <source>Tips: The clearer the image is, the more accurate the text is</source>
+        <translation>ຄ່ານິຍົມ: ການພິມທີ່ຊັດເຈນກວ່າ, ການພິມທີ່ຖືກຕ້ອງກວ່າ</translation>
+    </message>
+</context>
+</TS>
