188da9da74ad218aaf6be0e7730c38ceea403997
lichaofan <lichaofan@uniontech.com>
2025-09-17 13:21:14 +0800

    feat: Add DConfig source code.
    feat: 添加 DConfig 相关源代码。

---
+++ src/utils/dconfigmanager.cpp
@@ -0,0 +1,162 @@
+// SPDX-FileCopyrightText: 2022 - 2023 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "dconfigmanager.h"
+#include "private/dconfigmanager_p.h"
+
+#include <DConfig>
+
+#include <QDebug>
+
+static constexpr char kCfgAppId[] { "deepin-ocr" };
+
+DCORE_USE_NAMESPACE
+
+DConfigManager::DConfigManager(QObject *parent)
+    : QObject(parent), d(new DConfigManagerPrivate(this))
+{
+
+}
+
+DConfigManager *DConfigManager::instance()
+{
+    static DConfigManager ins;
+    return &ins;
+}
+
+DConfigManager::~DConfigManager()
+{
+#ifdef DTKCORE_CLASS_DConfig
+    QWriteLocker locker(&d->lock);
+
+    auto configs = d->configs.values();
+    std::for_each(configs.begin(), configs.end(), [](DConfig *cfg) { delete cfg; });
+    d->configs.clear();
+#endif
+}
+
+bool DConfigManager::addConfig(const QString &config, QString *err)
+{
+#ifdef DTKCORE_CLASS_DConfig
+    QWriteLocker locker(&d->lock);
+
+    if (d->configs.contains(config)) {
+        if (err)
+            *err = "config is already added";
+        return false;
+    }
+
+    auto cfg = DConfig::create(kCfgAppId, config, "", this);
+    if (!cfg) {
+        if (err)
+            *err = "cannot create config";
+        return false;
+    }
+
+    if (!cfg->isValid()) {
+        if (err)
+            *err = "config is not valid";
+        delete cfg;
+        return false;
+    }
+
+    d->configs.insert(config, cfg);
+    locker.unlock();
+    connect(cfg, &DConfig::valueChanged, this, [=](const QString &key) { Q_EMIT valueChanged(config, key); });
+#endif
+    return true;
+}
+
+bool DConfigManager::removeConfig(const QString &config, QString *err)
+{
+#ifdef DTKCORE_CLASS_DConfig
+    QWriteLocker locker(&d->lock);
+
+    if (d->configs.contains(config)) {
+        delete d->configs[config];
+        d->configs.remove(config);
+    }
+#endif
+    return true;
+}
+
+QStringList DConfigManager::keys(const QString &config) const
+{
+#ifdef DTKCORE_CLASS_DConfig
+    QReadLocker locker(&d->lock);
+
+    if (!d->configs.contains(config))
+        return QStringList();
+
+    return d->configs[config]->keyList();
+#else
+    return QStringList();
+#endif
+}
+
+bool DConfigManager::contains(const QString &config, const QString &key) const
+{
+    return key.isEmpty() ? false : keys(config).contains(key);
+}
+
+QVariant DConfigManager::value(const QString &config, const QString &key, const QVariant &fallback) const
+{
+#ifdef DTKCORE_CLASS_DConfig
+    QReadLocker locker(&d->lock);
+
+    if (d->configs.contains(config)) {
+        // 如果DConfig没刷新，存在有config但没key的情况
+        if (!this->contains(config, key)) {
+            qWarning() << "DConfig has no key:" << key << "in config:" << config << "REBOOT may fix it";
+            return fallback;
+        }
+
+        //qDebug() << "dconfig:" << config << "key:" << key << "value:" << d->configs.value(config)->value(key, fallback);
+        return d->configs.value(config)->value(key, fallback);
+    }
+    else
+        qWarning() << "Config:" << config << "is not registered!!!";
+    return fallback;
+#else
+    return fallback;
+#endif
+}
+
+void DConfigManager::setValue(const QString &config, const QString &key, const QVariant &value)
+{
+#ifdef DTKCORE_CLASS_DConfig
+    QReadLocker locker(&d->lock);
+
+    if (d->configs.contains(config)) {
+        // 如果DConfig没刷新，存在有config但没key的情况
+        if (!this->contains(config, key)) {
+            qWarning() << "DConfig has no key:" << key << "in config:" << config << "REBOOT may fix it";
+            return;
+        }
+
+        qInfo() << "dconfig:" << config << "key:" << key << "value:" << value;
+        d->configs.value(config)->setValue(key, value);
+    } else {
+        qWarning() << "Config:" << config << "is not registered!!!";
+    }
+#endif
+}
+
+bool DConfigManager::validateConfigs(QStringList &invalidConfigs) const
+{
+#ifdef DTKCORE_CLASS_DConfig
+    QReadLocker locker(&d->lock);
+
+    bool ret = true;
+    for (auto iter = d->configs.cbegin(); iter != d->configs.cend(); ++iter) {
+        bool valid = iter.value()->isValid();
+        if (!valid)
+            invalidConfigs << iter.key();
+        ret &= valid;
+    }
+    return ret;
+#else
+    return true;
+#endif
+}
